714ef474a5b5f002e785b2a555edd9eee4a5f48b
Sridhar Vumma - I71645 <Sridhar.Vumma1@microchip.com>
2022-09-24 19:23:01 +0530

    First set of files

---
+++ PIC24FJ64GU205_Automated.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC24FJ64GU205_Automated.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC24FJ64GU205_Automated.X.production.hex
